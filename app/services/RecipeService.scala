0ea290a86c4d322ee4bd5399cd9bdfbbc12356f7
Luke Alford <luke.alford@tstllc.net>
2014-08-03 23:22:28 -0400

    embedded recipes are checked for existence before being added to another recipe

--- app/services/RecipeService.scala
+++ app/services/RecipeService.scala
@@ -5,7 +5,8 @@ import play.api.Logger
 import reactivemongo.api.collections.default.BSONCollection
 import reactivemongo.bson.BSONDocument
 
-import scala.concurrent.Future
+import scala.concurrent.{Promise, Future}
+import scala.util.{Failure, Success}
 
 class RecipeService extends MongoDataSource {
   override val dbName = "recipe-manager"
@@ -35,7 +36,7 @@ class RecipeService extends MongoDataSource {
 
   def insertRecipe(recipe: Recipe) = {
     findRecipe(recipe.name) flatMap {
-      case None => {
+      case None => checkEmbeddedRecipes(recipe) {
         recipes.insert(recipe).flatMap { lastError =>
           Logger.debug(s"inserted with LastError: $lastError")
           if (lastError.ok)
@@ -50,7 +51,7 @@ class RecipeService extends MongoDataSource {
     }
   }
 
-  def updateRecipe(recipe: Recipe) = {
+  def updateRecipe(recipe: Recipe) = checkEmbeddedRecipes(recipe) {
     recipes
       .update(BSONDocument("name" -> recipe.name), recipe)
       .flatMap { lastError =>
@@ -62,5 +63,24 @@ class RecipeService extends MongoDataSource {
           Future.failed(lastError)
         }
     }
+  }
+
+  private def checkEmbeddedRecipes(recipe: Recipe)(persistRecipe: => Future[Recipe]) = {
+    val recipeNames = recipe.recipeIngredients.map(_.name)
+    val checkedEmbeddedRecipesFutures = recipeNames map { name =>
+      val existingRecipesFuture = Future.traverse(recipeNames) { name => findRecipe(name) }
+      existingRecipesFuture flatMap { existingRecipes =>
+        if (existingRecipes.contains(None)) Future.failed(new IllegalArgumentException(s"embedded recipes for ${recipe.name} contained one or more entries that do not exist, recipes must be created before being embedded"))
+        else Future.successful()
+      }
+    }
+
+    val recipePromise = Promise[Recipe]()
+    Future.sequence(checkedEmbeddedRecipesFutures).onComplete {
+      case Success(_) => recipePromise.completeWith(persistRecipe)
+      case Failure(t) => recipePromise.failure(t)
+    }
+
+    recipePromise.future
   }
 }
