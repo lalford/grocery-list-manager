fe4bbbef5433cbdc2a513cf976d7a92661f8036c
Luke Alford <luke.alford@tstllc.net>
2014-09-01 23:56:05 -0400

    backend support for displaying one embedded level of recipes in the sidebar for active grocery list, display styling needs work.

--- app/controllers/ActionHelper.scala
+++ app/controllers/ActionHelper.scala
@@ -1,55 +1,95 @@
 package controllers
 
+import controllers.ActionConstants.RecipePlusOneEmbedLevel
 import models.GroceryList
-import play.api.mvc.{WrappedRequest, ActionBuilder, SimpleResult, Request}
-import services.GroceryListService
+import play.api.mvc._
+import services.{RecipeService, GroceryListService}
 import scala.concurrent.Future
 import play.api.libs.concurrent.Execution.Implicits.defaultContext
 
 object ActionHelper extends ActionBuilder[RequestContext] {
   val groceryListService = new GroceryListService
+  val recipeService = new RecipeService
 
   def invokeBlock[A](request: Request[A], block: (RequestContext[A]) => Future[SimpleResult]) = {
+    val activeGroceryListNameOpt = request.session.get(ActionConstants.activeGroceryListKey)
     val currentUri = request.uri
     val currentQueryStr = request.rawQueryString
     val redirectUrl = s"$currentUri$currentQueryStr"
 
-    groceryListService.findGroceryLists flatMap { groceryLists =>
-      val sorted = groceryLists.sortWith(_.name.toLowerCase < _.name.toLowerCase)
-      val groceryListActivators = sorted.map(gl => GroceryListActivator(gl.name, routes.GroceryListController.makeActiveGroceryList(gl.name, redirectUrl).url))
+    val requestContextFuture = for {
+      groceryLists <- groceryListService.findGroceryLists
+      groceryListActivators <- getGroceryListActivators(groceryLists, redirectUrl)
+      activeGroceryList <- getActiveGroceryList(groceryLists, activeGroceryListNameOpt)
+      navigationLinks <- getNavigationLinks(redirectUrl)
+    } yield RequestContext(
+      request,
+      groceryListActivators,
+      activeGroceryList,
+      navigationLinks
+    )
 
-      val activeGroceryList = for {
-        name <- request.session.get(ActionConstants.activeGroceryListKey)
+    requestContextFuture flatMap block
+  }
+
+  private def getGroceryListActivators(groceryLists: List[GroceryList], redirectUrl: String): Future[List[GroceryListActivator]] = Future {
+    val sorted = groceryLists.sortWith(_.name.toLowerCase < _.name.toLowerCase)
+    val groceryListActivators = sorted.map(gl => GroceryListActivator(gl.name, routes.GroceryListController.makeActiveGroceryList(gl.name, redirectUrl).url))
+    groceryListActivators
+  }
+
+  private def getNavigationLinks(redirectUrl: String): Future[NavigationLinks] = Future {
+    NavigationLinks(
+      lastUrl = redirectUrl,
+      newGroceryListUrl = routes.GroceryListController.newGroceryList.url,
+      viewGroceryListsUrl = routes.GroceryListController.viewGroceryLists.url,
+      newRecipeUrl = routes.RecipeController.newRecipe.url,
+      viewRecipesUrl = routes.RecipeController.viewRecipes.url
+    )
+  }
+
+  private def getActiveGroceryList(groceryLists: List[GroceryList], activeGroceryListNameOpt: Option[String]): Future[Option[ActiveGroceryList]] = {
+    val groceryListOptFuture: Future[Option[GroceryList]] = Future {
+      for {
+        name <- activeGroceryListNameOpt
         gl <- groceryLists.find(_.name == name)
-      } yield ActiveGroceryList(gl, routes.GroceryListController.viewGroceryList(name).url, routes.GroceryListController.viewShoppingList(name).url)
-
-      val navigationLinks = NavigationLinks(
-        lastUrl = redirectUrl,
-        newGroceryListUrl = routes.GroceryListController.newGroceryList.url,
-        viewGroceryListsUrl = routes.GroceryListController.viewGroceryLists.url,
-        newRecipeUrl = routes.RecipeController.newRecipe.url,
-        viewRecipesUrl = routes.RecipeController.viewRecipes.url
-      )
-
-      val requestContext = RequestContext(
-        request,
-        groceryListActivators,
-        activeGroceryList,
-        navigationLinks
-      )
-
-      block(requestContext)
+      } yield gl
     }
+
+    val activeGroceryListOpt = for {
+      groceryListOpt <- groceryListOptFuture
+      recipeNamesPlus <- getRecipeNamesPlusOneEmbedLevel(groceryListOpt)
+    } yield {
+      groceryListOpt match {
+        case Some(gl) => Some(ActiveGroceryList(gl, routes.GroceryListController.viewGroceryList(gl.name).url, routes.GroceryListController.viewShoppingList(gl.name).url, recipeNamesPlus))
+        case _ => None
+      }
+    }
+
+    activeGroceryListOpt
+  }
+
+  private def getRecipeNamesPlusOneEmbedLevel(groceryListOpt: Option[GroceryList]): Future[List[RecipePlusOneEmbedLevel]] = groceryListOpt match {
+    case Some(groceryList) =>
+      Future.traverse(groceryList.recipeServings) { recipeServing =>
+        recipeService.findRecipe(recipeServing.name) map {
+          case recipeOpt if recipeOpt.isDefined => recipeOpt.get.name -> recipeOpt.get.recipeIngredients.map(_.name)
+          case _ => "" -> List()
+        }
+      }
+    case _ =>
+      Future.successful(List())
   }
 }
 
 object ActionConstants {
+  type RecipePlusOneEmbedLevel = (String, List[String])
   val activeGroceryListKey = "activeGroceryList"
 }
 
 case class GroceryListActivator(name: String, activatorUrl: String)
 
-case class ActiveGroceryList(groceryList: GroceryList, groceryListUrl: String, shoppingListUrl: String)
+case class ActiveGroceryList(groceryList: GroceryList, groceryListUrl: String, shoppingListUrl: String, recipeNamesPlusOneEmbedLevel: List[RecipePlusOneEmbedLevel])
 
 case class NavigationLinks(lastUrl: String,
                            newGroceryListUrl: String,
